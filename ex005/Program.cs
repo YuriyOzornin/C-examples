0cb2402dea5d9f810c347b06893bf35a4d032dac
Yuriy <ozornin-v@yandex.ru>
2022-10-01 09:09:51 +0500

    ex005

---
+++ ex005/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход
+// принимает число и выдаёт его квадрат (число
+// умноженное на само себя).
+// Например:
+// 4 -> 16
+// -3 -> 9
+// -7 -> 4
+ Console.Write("Enter number:");
+// convert = конвертирует string в другую переменную
+ int number = Convert.ToInt32(Console.ReadLine());
+
+ int value = number * number;
+
+ System.Console.WriteLine("square number:"+value);
