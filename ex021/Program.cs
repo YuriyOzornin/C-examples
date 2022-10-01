86bca9fcfb526b5b0b7657cfc82e080b32122417
Yuriy <ozornin-v@yandex.ru>
2022-10-01 09:50:13 +0500

    ex021

---
+++ ex021/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным 
+// и минимальным элементов массива.
+
+double[] array = new double[15];
+void MinMaxArray(double[] arr)
+{
+  double MinNum= 0;
+  double MaxNum= 0;
+  
+  for(int i = 0; i < arr.Length; i++)
+ {
+    arr[i] = new Random().Next(-5,25);
+    Console.Write(arr[i]+" ");
+ }
+   for (int i = 1; i < arr.Length; i++)
+ {
+    if (arr[i] < MinNum) MinNum = arr[i];
+    if (arr[i] > MaxNum) MaxNum = arr[i];
+     
+ }
+     Console.WriteLine($"Min number array {MinNum}\nMax number array {MaxNum}");
+     Console.WriteLine($"{MaxNum - MinNum}");
+}
+MinMaxArray(array);
