ffc8e15d65991bb602ec6f703598818e9f9a7fe6
Yuriy <ozornin-v@yandex.ru>
2022-10-01 09:26:07 +0500

    ex014

---
+++ ex013/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+ Console.WriteLine("Enter x1...");
+ int x1 = int.Parse(Console.ReadLine());
+
+ Console.WriteLine("Enter y1...");
+ int y1 = int.Parse(Console.ReadLine());
+
+ Console.WriteLine("Enter z1...");
+ int z1 = int.Parse(Console.ReadLine());
+
+ Console.WriteLine("Enter x2...");
+ int x2 = int.Parse(Console.ReadLine());
+
+ Console.WriteLine("Enter y2...");
+ int y2 = int.Parse(Console.ReadLine());
+
+ Console.WriteLine("Enter z2...");
+ int z2 = int.Parse(Console.ReadLine());
+
+ double result = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2)+ Math.Pow((y2 - y1), 2));
+ Console.WriteLine("длинна отрезка"+result);
+
+ Console.WriteLine($"A ({x1},{y1},{z2}); B ({x2},{y2},{z2}) -> {result}");
+
---
+++ ex014/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+ Console.WriteLine("Enter number...");
+ int number = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine("Таблица кубов");
+
+ for(int i = 1;i<=number;i++)
+ {
+         Console.WriteLine(Math.Pow(i,3));
+         Console.WriteLine("");
+        
+ }
