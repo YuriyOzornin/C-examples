757b3536b1547a06a0c4552d0ebe4eb37a24d218
Yuriy <ozornin-v@yandex.ru>
2022-10-01 09:24:51 +0500

    ex011

---
+++ ex011/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа 
+//или сообщает, что третьей цифры нет.
+
+ int number = new Random().Next(1, 100000);
+ Console.WriteLine(number);
+
+ if (number > 99)
+ {
+     string number1 = "";
+     number1 = Convert.ToString(number);
+     Console.WriteLine(number1[2]);
+ }
+ else
+ {
+     Console.WriteLine("This number is not have thirdiget");
+ }
