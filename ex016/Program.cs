913a8d3ecc18eff8dceb13fd666b496d953afb5c
Yuriy <ozornin-v@yandex.ru>
2022-10-01 09:27:54 +0500

    ex016

---
+++ ex016/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.WriteLine("Enter number...");
+void Progressive()
+{
+    Console.WriteLine("number A");
+    int numberA = Convert.ToInt32(Console.ReadLine()); 
+    Console.WriteLine("number B");
+    int numberB = Convert.ToInt32(Console.ReadLine());
+    int result = numberA;
+   for(int i=1; i<= numberB;i++)
+   {
+       result *= numberA;
+       Console.WriteLine(result);      
+   }
+   Console.WriteLine(result);
+}
+Progressive();
